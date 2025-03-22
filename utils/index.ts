603955ce0c6c9bc9471d87d73acf9e3c1678ab5c
Aadithyakp <71168518+Aadithyakp@users.noreply.github.com>
2025-03-22 13:38:04 -0400

    Update index.ts took away the limit parameter from the url

--- utils/index.ts
+++ utils/index.ts
@@ -9,7 +9,7 @@ export async function fetchCars(filters: FilterProps){
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
       };
 
-      const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${filters.manufacturer}&model=${filters.model}&year=${filters.year}&limit=${filters.limit}&fuel_type=${filters.fuel}`,{
+      const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${filters.manufacturer}&model=${filters.model}&year=${filters.year}&fuel_type=${filters.fuel}`,{
         headers: headers,
       });
       const result = await response.json();
